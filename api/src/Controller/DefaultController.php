86e2b76edaeeb618ef40c7f629d673483d55ee8b
Ruben van der Linde <ruben@conduction.nl>
2021-01-04 08:41:58 +0100

    Home page activation

--- api/src/Controller/DefaultController.php
+++ api/src/Controller/DefaultController.php
@@ -34,9 +34,9 @@ class DefaultController extends AbstractController
     public function indexAction(CommonGroundService $commonGroundService, MailingService $mailingService, Request $request, ParameterBagInterface $params)
     {
         $variables = [];
-        $variables['events'] = $commonGroundService->getResource(['component' => 'arc', 'type' => 'events']);
-        $variables['groups'] = $commonGroundService->getResource(['component' => 'pdc', 'type' => 'groups']);
-
+        $variables['events'] = $commonGroundService->getResourceList(['component' => 'arc', 'type' => 'events'])['hydra:member'];
+        $variables['groups'] = $commonGroundService->getResourceList(['component' => 'pdc', 'type' => 'groups'])['hydra:member'];
+        //$variables['locations'] = $commonGroundService->getResourceList(['component' => 'lc', 'type' => 'accommodations'])['hydra:member'];
 
         return $variables;
     }
