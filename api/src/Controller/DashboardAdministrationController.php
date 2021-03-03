266b62edf48010bc9000b0eb57ad9c3c7a0305b3
Mark Bout <markhendrikbout@gmail.com>
2021-03-03 11:57:06 +0100

    wip global administration

---
+++ api/src/Controller/DashboardAdministrationController.php
@@ -0,0 +1,72 @@
+<?php
+
+// src/Controller/DefaultController.php
+
+namespace App\Controller;
+
+use Conduction\CommonGroundBundle\Service\CommonGroundService;
+use Conduction\IdVaultBundle\Service\IdVaultService;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * The DashboardController handles any calls about administration and dashboard pages.
+ *
+ * Class DashboardController
+ *
+ * @Route("/dashboard/administration")
+ */
+
+class DashboardAdministrationController extends AbstractController
+{
+    /**
+     * @Route("/")
+     * @Template
+     */
+    public function indexAction(CommonGroundService $commonGroundService, Request $request, IdVaultService $idVaultService, ParameterBagInterface $params)
+    {
+        $variables = [];
+        // Make sure the user is logged in
+        if (!$this->getUser()) {
+            return $this->redirect($this->generateUrl('app_user_idvault'));
+        }
+        return $variables;
+    }
+
+    /**
+     * @Route("/organizations")
+     * @Template
+     */
+    public function organizationsAction(Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params, IdVaultService $idVaultService)
+    {
+        $variabeles = [];
+
+        return $variabeles;
+    }
+
+    /**
+     * @Route("/locations")
+     * @Template
+     */
+    public function locationsAction(Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params, IdVaultService $idVaultService)
+    {
+        $variabeles = [];
+
+        return $variabeles;
+    }
+
+    /**
+     * @Route("/categories")
+     * @Template
+     */
+    public function categoriesAction(Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params, IdVaultService $idVaultService)
+    {
+        $variabeles = [];
+
+        return $variabeles;
+    }
+}
