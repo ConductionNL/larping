11f37c946aaa312fe5cb7dd91cb4093460bd4423
ginokok1996 <ginokok1996@hotmail.com>
2021-04-13 11:58:33 +0200

    app container

---
+++ app/src/App.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import './App.css';
+
+
+export default class App extends Component {
+
+  constructor(props) {
+    super();
+    let api = null;
+    let url = null;
+
+    if (window.location.href.includes('http://localhost')) {
+      url ='http://localhost';
+      api = 'http://localhost:83/api';
+    }
+    else
+    {
+      url= 'https://www.dashkube.com';
+      api ='https://www.dashkube.com/api';
+    }
+
+    this.state = {
+      url: url,
+      api: api,
+    }
+  }
+
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <p>App.js</p>
+    );
+  }
+
+}
+
